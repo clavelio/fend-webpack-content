1334a4ad6702abd799772b67f64b8ab41a92a0a2
clow <claudioramirezjimenez@gmail.com>
2021-01-18 18:32:32 +0000

    minor changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,15 @@
 const path = require('path')
 const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
     entry: './src/client/index.js',
     devtool: 'source-map',
     module: {
-        rules: [
-            {
-                test: '/\.js$/',
-                exclude: /node_modules/,
-                loader: "babel-loader"
-            }
-        ]
+        rules: [{
+            test: '/\.js$/',
+            exclude: /node_modules/,
+            loader: "babel-loader"
+        }]
     }
 }
